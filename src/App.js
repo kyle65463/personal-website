3301fe0a4e33c8a477852bb27ef6d5fcdf9f3156
kyle65463 <kyle65463@gmail.com>
2021-03-21 23:05:13 +0800

    fix: router basename

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
     return (
-        <Router>
+        <Router basename={ process.env.PUBLIC_URL }>
             <div className="App">
                 <Navbar />
                     <Route path="/" exact component={Home} />
